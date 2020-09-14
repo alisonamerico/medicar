0ebc61a507eaa8af7632f255897dc9351aedd755
alisonamerico <alison.americo@gmail.com>
2020-09-14 12:57:35 -0300

    Extracted configuration from variable 'DEBUG'

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '(^wl=#(teod&j)^9b2#uqsdqpd%71+5&719ojp2dpzfpijoy6x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
